c84ed9bbcb21f818ae3bf0d012c1a076dd65d0f4
Vladislav Aleksandrov <happymushroom93@gmail.com>
2022-01-18 19:15:52 +0300

    builded

--- src/Components/Footer/Footer.js
+++
@@ -1,7 +0,0 @@
-import s from './Footer.module.css'
-
-export const Footer = () => {
-	return (
-		<img className={s.img} src="/img/onpoint.svg" alt="onpOint"/>
-	)
-}
--- src/Components/Slides/ThirdSlide/Title/Title.js
+++
@@ -1,9 +0,0 @@
-export const Title = ({title, s}) => {
-	return (
-		<div className={s.textBlock}>
-			<p className={s.title}>{title}</p>
-			<br/>
-			<h1 className={s.brend}>BREND <span className={s.extraBold}> XY</span></h1>
-		</div>
-	)
-}
